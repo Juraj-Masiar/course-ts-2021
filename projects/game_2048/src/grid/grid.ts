1e033b5e5f82b8611a72b8707ddf41f2a6d04987
Juraj Mäsiar <juraj.masiar@gmail.com>
2021-11-11 14:32:48 +0100

    - added "jest" for tests

---
+++ projects/game_2048/src/grid/grid.ts
@@ -0,0 +1,12 @@
+type Direction = 'left' | 'right' | 'up' | 'down';
+
+interface Grid {
+
+}
+
+
+export function moveGrid(grid: Grid, direction: Direction): Grid {
+
+  return {};
+}
+
---
+++ projects/game_2048/test/grid_operations.test.ts
@@ -0,0 +1,15 @@
+import {moveGrid} from '../src/grid/grid';
+
+test('move numbers right', () => {
+
+  const inputGrid = {
+
+  };
+  const result = moveGrid(inputGrid, 'right');
+
+  const expectedGrid = {
+
+  };
+
+  expect(result).toEqual(expectedGrid);
+})
