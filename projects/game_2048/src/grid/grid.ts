7cc8b25c1e98a18038fbf3b170fcc0ab740d738d
juraj <juraj.masiar@gmail.com>
2021-11-11 16:53:35 +0100

    - added grid interface - added first test for move operation

--- projects/game_2048/src/grid/grid.ts
+++ projects/game_2048/src/grid/grid.ts
@@ -1,12 +1,29 @@
 type Direction = 'left' | 'right' | 'up' | 'down';
 
-interface Grid {
-
+export interface Grid {
+  width: number,
+  height: number,
+  data: [
+    [number, number, number, number],
+    [number, number, number, number],
+    [number, number, number, number],
+    [number, number, number, number],
+  ],
 }
 
 
 export function moveGrid(grid: Grid, direction: Direction): Grid {
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
-  return {};
+  return {
+    data: [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0]
+    ],
+    height: 0,
+    width: 0,
+  };
 }
 
--- projects/game_2048/test/grid_operations.test.ts
+++ projects/game_2048/test/grid_operations.test.ts
@@ -1,14 +1,28 @@
-import {moveGrid} from '../src/grid/grid';
+import {Grid, moveGrid} from '../src/grid/grid';
 
 test('move numbers right', () => {
 
-  const inputGrid = {
-
+  const inputGrid: Grid = {
+    data: [
+      [0, 2, 2, 0],
+      [0, 8, 2, 0],
+      [0, 0, 0, 0],
+      [4, 4, 4, 4],
+    ],
+    height: 0,
+    width: 0
   };
   const result = moveGrid(inputGrid, 'right');
 
-  const expectedGrid = {
-
+  const expectedGrid: Grid = {
+    data: [
+      [0, 0, 0, 4],
+      [0, 0, 8, 2],
+      [0, 0, 0, 0],
+      [0, 0, 8, 8],
+    ],
+    height: 0,
+    width: 0
   };
 
   expect(result).toEqual(expectedGrid);
